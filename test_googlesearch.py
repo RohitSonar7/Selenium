4b1aa8cdddafc559822b35cc5f374c0b5a00af81
unknown <sonarrohit7@gmail.com>
2022-03-30 15:18:17 +0530

    Commit

--- test_googlesearch.py
+++ test_googlesearch.py
@@ -1,5 +1,4 @@
 import time
-
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -7,6 +6,8 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 print("Test Case has Started")
 driver.maximize_window()
 driver.get("https://www.google.com/")
+time.sleep(1)
+driver.find_element_by_name("q").send_keys("Harman")
 time.sleep(5)
 driver.close()
 print("Test has been completed successfully!!")
